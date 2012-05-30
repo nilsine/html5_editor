cdc6c4a70ba9e304bada5dfab73409debbb237dd
Jean-philippe Lannoy <jp.lannoy@nilsine.fr>
2012-05-30 18:07:42 +0200

    Smarter update of the hidden textarea

--- jquery.html5_editor.js
+++ jquery.html5_editor.js
@@ -18,6 +18,23 @@
 (function($){var a={},c="doTimeout",d=Array.prototype.slice;$[c]=function(){return b.apply(window,[0].concat(d.call(arguments)))};$.fn[c]=function(){var f=d.call(arguments),e=b.apply(this,[c+f[0]].concat(f));return typeof f[0]==="number"||typeof f[1]==="number"?this:e};function b(l){var m=this,h,k={},g=l?$.fn:$,n=arguments,i=4,f=n[1],j=n[2],p=n[3];if(typeof f!=="string"){i--;f=l=0;j=n[1];p=n[2]}if(l){h=m.eq(0);h.data(l,k=h.data(l)||{})}else{if(f){k=a[f]||(a[f]={})}}k.id&&clearTimeout(k.id);delete k.id;function e(){if(l){h.removeData(l)}else{if(f){delete a[f]}}}function o(){k.id=setTimeout(function(){k.fn()},j)}if(p){k.fn=function(q){if(typeof p==="string"){p=g[p]}p.apply(m,d.call(n,i))===true&&!q?o():e()};o()}else{if(k.fn){j===undefined?e():k.fn(j===false);return true}else{e()}}}})(jQuery);
 
 
+/*
+ * Add a change event on contenteditable elements
+ * From http://stackoverflow.com/a/6263537/270433
+ */
+$('[contenteditable]').live('focus', function() {
+    var $this = $(this);
+    $this.data('before', $this.html());
+    return $this;
+}).live('blur keyup paste', function() {
+    var $this = $(this);
+    if ($this.data('before') !== $this.html()) {
+        $this.data('before', $this.html());
+        $this.trigger('change');
+    }
+    return $this;
+});
+
 	
 (function($) {
 
@@ -159,9 +176,11 @@
 					});
 				}
 				
-				$contenteditable.blur(function() {
+				$contenteditable.change(function() {
 					$this.val($(this).html());
-					
+				});
+				
+				$contenteditable.blur(function() {
 					if (settings['auto-hide-toolbar']) {
 						/*
 						 * if ather 250ms the toolbar was not clicked, hide it
